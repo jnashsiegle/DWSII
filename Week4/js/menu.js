365164f49eb3cacf7c5e8a09c637b89cd8d8bdc9
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-02-23 17:01:11 -0600

    ...

--- Week4/js/menu.js
+++ Week4/js/menu.js
@@ -1,17 +1,17 @@
-$(function() {
-			var pull 		= $('#pull');
-				menu 		= $('nav ul');
-				menuHeight	= menu.height();
+$(document).ready(function() {  
+    var pull        = $('#pull');   
+    var menu        = $('nav ul'); // don't miss the var
+    var menuHeight  = menu.height();  // don't miss the var
 
-			$(pull).on('click', function(e) {
-				e.preventDefault();
-				menu.slideToggle();
-			});
+    $(pull).on('click', function(e) {  
+        e.preventDefault();  
+        menu.slideToggle();  
+    });  
 
-			$(window).resize(function(){
-        		var w = $(window).width();
-        		if(w > 320 && menu.is(':hidden')) {
-        			menu.removeAttr('style');
-        		}
-    		});
-		});
+    $(window).resize(function(){  
+        var w = $(window).width();  
+        if(w > 320 && menu.is(':hidden')) {  
+            menu.removeAttr('style');  
+        }  
+    });
+}); // close
